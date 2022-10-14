f550184dbddf37ab9dde270f5e7f466290cb37ff
guptasonu14 <sguptabudha@gmail.com>
2022-10-14 11:48:18 +0530

    Introduction to object

---
+++ Fundamentals-Part2/object.js
@@ -0,0 +1,17 @@
+/////////Introduction object /////////////
+const jonasArray = [
+    'jonas',
+    'sonu',
+    2037-1991,
+    'teacher',
+    ['Michael','Peter','Steven']
+];
+
+const jonas = {
+    firstName: 'jonas',
+    lastName: 'gupta',
+    age:  2037-1991,
+    job: 'teacher',
+    friends:  ['Michael','Peter','Steven']
+
+};
